bf9aa5b9d85cd15863a4d2f1b1c9a80f4293f7b8
Leandro <leandrocorneo5@unesc.net>
2024-10-23 16:15:21 -0300

    add

--- web/session.php
+++ web/session.php
@@ -1,19 +1,19 @@
-<?php 
-    if(isset($_COOKIE['nome'])){
-        $nome = $_COOKIE['nome'];
-    }
+<?php
+    session_start();
+
+    $_SESSION['nome'] = "teste";
+
+    print_r($_SESSION);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php if($nome != ""): ?>
-        <h1>nome : <?= $nome ?></h1>
-    <?php endif ?>
+    <h1>teste</h1>
 </body>
 </html>
